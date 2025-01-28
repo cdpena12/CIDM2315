dafdfdcb9a60240dd6d984e85937fd279ae47f56
cdpena12 <cdpena1@buffs.wtamu.edu>
2025-01-27 21:50:06 -0600

    Code file for Homework1

---
+++ Homework1.cs
@@ -0,0 +1,18 @@
+﻿namespace Homework1;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        // The code for the Homework1
+        
+        double X = 2.5; 
+        double Y = 3.3;
+        Console.WriteLine(4*X*X + 3*Y);
+
+
+    
+
+        
+    }
+}
