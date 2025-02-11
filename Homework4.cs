1e68ed6bf77e1a95fdf1c31a5c29569d233b446e
cdpena12 <cdpena1@buffs.wtamu.edu>
2025-02-10 21:05:48 -0600

    Add files via upload

---
+++ Homework4.cs
@@ -0,0 +1,22 @@
+﻿namespace Homework4;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        // Question 1 method()
+
+        int a = 3;
+        int b = 5;
+
+        int largest = (a > b) ? a : b;
+
+        Console.WriteLine(" The largest number is " + largest);
+
+    
+    }
+
+    
+}
+
+
